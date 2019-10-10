57ce958dcf36e100c5af89fc3b75bbc5ffad995e
rosaito <m.general@alumnos.duoc.cl>
2019-10-10 15:34:02 -0300

    Repositorio crud

---
+++ indice.py
@@ -0,0 +1,31 @@
+print ("------------------------------")
+print ("1.Agregar")
+print ("2.Eliminar")
+print ("3.Listar")
+print ("4.Modificar")
+print ("5.Salir")
+print ("------------------------------")
+
+
+seleccion = None
+nombre = None
+apellido = None
+edad = None
+while seleccion != 5:
+    seleccion = input("Ingrese un valor: ")
+    if seleccion == "1":
+        print("Agregar")
+        nombre = input("Ingresa tu nombre: ")
+        apellido = input("Ingresa tu apellido: ")
+        edad = input("Ingresa tu edad: ")
+        print("Tus datos son: "+nombre+" "+apellido+" "+edad+" años")
+    elif seleccion == "2":
+        print("Eliminar")
+    elif seleccion == "3":
+        print("Listar")
+    elif seleccion == "4":
+        print("Modificar")
+    elif seleccion=="5":
+        print("Salir")
+        break
+exit()
